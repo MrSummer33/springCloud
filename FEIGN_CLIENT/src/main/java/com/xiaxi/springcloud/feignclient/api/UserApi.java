87e5599838140b713d0fa8e7a91e9b23c9f59510
Xi Xia <18296154779@163.com>
2018-08-29 09:43:48 +0800

    hystrix

--- FEIGN_CLIENT/src/main/java/com/xiaxi/springcloud/feignclient/api/UserApi.java
+++ FEIGN_CLIENT/src/main/java/com/xiaxi/springcloud/feignclient/api/UserApi.java
@@ -1,9 +1,10 @@
 package com.xiaxi.springcloud.feignclient.api;
 
 import com.xiaxi.dto.UserDto;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by BaoCai on 2018/8/13. Description:
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "feign-user")
 public interface UserApi {
 
-    @GetMapping("/find_user/{user_id}")
+    @RequestMapping(value = "/find_user/{user_id}", method = RequestMethod.GET)
     UserDto getByUser(@PathVariable(value = "user_id") Long userId);
 
 }
