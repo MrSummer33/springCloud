0b2644059a3cf1ade51aa465582c1e488debf210
xiaxi <18296154779@163.com>
2019-08-21 19:29:01 +0800

    erueka mutil zone

--- FEIGN_USER/src/main/java/com/xiaxi/springcloud/feignuser/controller/UserController.java
+++ FEIGN_USER/src/main/java/com/xiaxi/springcloud/feignuser/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.xiaxi.springcloud.feignuser.controller;
 
 import com.xiaxi.dto.UserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Value("${spring.profiles:11111}")
+    private String zone;
+
     @GetMapping("/find_user/{user_id}")
     public UserDto getUserById(@PathVariable(value = "user_id") Long userId){
-        return UserDto.instance("科比",userId);
+        return UserDto.instance(zone,userId);
     }
 
 }
